505772b494da5d97c6f79586cfa608b5ca039a3a
hanifan <hanifannurulhaq@outlook.com>
2020-08-28 21:07:42 +0700

    tinggaleun fungsi cek na wkwkwkwkwk

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -7,8 +7,12 @@ class User extends CI_Controller
 	{
 		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 		$username['nama_lengkap'] = $data['user']['nama_lengkap'];
-		$this->load->view('home/home_header');
-		$this->load->view('home/home', $username);
-		$this->load->view('home/home_footer');
+		if($this->session->userdata('email') == ''){
+			redirect('auth');
+		}else{
+			$this->load->view('home/home_header');
+			$this->load->view('home/home', $username);
+			$this->load->view('home/home_footer');
+		}
 	}
 }
